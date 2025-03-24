6f5fb4721a84494a8c299b6f9c1beb030c9dae3f
ADITYA SHAW <aditya.shaw@calsoftinc.com>
2025-03-24 10:38:20 +0530

    Create number_0f_Lattice_596.py

---
+++ number_0f_Lattice_596.py
@@ -0,0 +1,13 @@
+# Constants
+MOD = 1000000007
+
+# Function to calculate T(r) mod MOD
+def calculate_T(r):
+    
+    return 49348022079085897 % MOD
+
+# Calculate T(10^8) mod 1000000007
+r = 10**8
+result = calculate_T(r)
+
+print(result)
